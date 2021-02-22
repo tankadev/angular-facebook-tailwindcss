8b3ab553847a53f8fdd91ed78cb3513875052594
Van Nguyen <nguyentankadev@gmail.com>
2021-02-22 22:03:35 +0700

    done menu left, update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,15 +13,20 @@ module.exports = {
           'dark-second': '#242526',
           'dark-third': '#3A3B3C',
           'dark-fourth': '#535557',
+          'dark-fifth': '#191A1B',
+          'dark-sixth': '#303132',
           'dark-txt': '#B8BBBF'
         }
       },
       borderWidth: {
+        '1': '1px',
         '3': '3px'
       }
     },
     variants: {
-      extend: {},
+      extend: {
+        display: ['group-hover']
+      },
     },
     plugins: [],
 };
